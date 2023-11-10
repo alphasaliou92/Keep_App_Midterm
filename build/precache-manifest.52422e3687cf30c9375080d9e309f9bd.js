50d377758751babc3721d7b2f657f1f5f5c644b0
alphasaliou92 <38296852+alphasaliou92@users.noreply.github.com>
2023-11-10 17:11:43 -0500

    html and css links commented out

---
+++ build/precache-manifest.52422e3687cf30c9375080d9e309f9bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293121516c6b408f3be41998f1d87b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3135051a2ca019c2144",
+    "url": "/static/js/2.6e9c32dd.chunk.js"
+  },
+  {
+    "revision": "0ebc03b06e6b08acc45f",
+    "url": "/static/js/main.8c092996.chunk.js"
+  },
+  {
+    "revision": "44e262517cacfb9bb9bb",
+    "url": "/static/js/runtime-main.5b72627c.js"
+  }
+]);
